660cf846f8ac16507a323c2078d0633b507ace4a
kostytsAleksandr <kostyts.aleksandr@gmail.com>
2020-12-17 13:56:06 +0200

    EmtyProj

---
+++ MyListConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyListConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
